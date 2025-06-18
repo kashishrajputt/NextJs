428540d04e24e0dacf6a434436dc9183507bf6b8
kashishrajputt <btecg41522029@dseu.ac.in>
2025-06-18 19:29:54 +0530

    nextjs basics

---
+++ app/(dashboard)/dashboard/analytics/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      Analytics
+    </div>
+  )
+}
+
+export default page
---
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const page = ({ params }:{ params: { id:string }}) => {
+    const { id } = params;
+  return (
+    <div>
+      <h1>user profile : {id}</h1>
+    </div>
+  )
+}
+
+export default page
